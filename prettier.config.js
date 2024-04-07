14c21b19ffbff8c3e05411354e6676fde9834597
David Asiamah <daveasiamah@gmail.com>
2024-04-07 09:06:59 +0000

    initial commit

--- prettier.config.js
+++ prettier.config.js
@@ -3,4 +3,16 @@ const styleguide = require('@vercel/style-guide/prettier');
 module.exports = {
   ...styleguide,
   plugins: [...styleguide.plugins, 'prettier-plugin-tailwindcss'],
+  overrides: [
+    {
+      files: ['*.tsx'],
+      options: {
+        ...styleguide.options,
+        proseWrap: 'preserve',
+        functionDeclaration: {
+          parameters: 'single',
+        },
+      },
+    },
+  ],
 };
